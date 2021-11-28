bf2edbd1c2866fbe336bc8ceb58e98b3aac34927
Ghassan Al-Mashareqa <kronikarz@gmail.com>
2021-11-28 11:20:27 +0100

    Add project files.

---
+++ windows_message_box.cpp
@@ -0,0 +1,127 @@
+#pragma once
+
+#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
+
+#include "windows_message_box.h"
+
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <locale>
+#include <codecvt>
+
+#define WIN32_LEAN_AND_MEAN
+#include <shlwapi.h>
+#include <shellapi.h>
+#pragma comment(lib, "Shlwapi.lib")
+#include <Commctrl.h>
+#pragma comment(lib, "Comctl32.lib")
+
+#ifdef _UNICODE
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_IA64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+
+namespace ghassanpl
+{
+	namespace
+	{
+		std::wstring utf8_to_utf16(std::string_view str)
+		{
+			std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+			return converter.from_bytes(str.data(), str.data() + str.size());
+		}
+	}
+
+	windows_message_box_result windows_message_box(windows_message_box_params const& param)
+	{
+		/// Set the common parameters
+		TASKDIALOGCONFIG task_config = {};
+		task_config.cbSize = sizeof(TASKDIALOGCONFIG);
+		task_config.hwndParent = param.window_handle ? (HWND)param.window_handle : GetActiveWindow();
+		task_config.hInstance = GetModuleHandle(nullptr);
+		task_config.dwFlags =
+			TDF_ENABLE_HYPERLINKS
+			| TDF_USE_COMMAND_LINKS
+			//| TDF_EXPANDED_BY_DEFAULT
+			| TDF_SIZE_TO_CONTENT;
+		task_config.dwCommonButtons = 0;
+		task_config.hMainIcon = nullptr;
+
+		/// No radiobuttons, no other platforms use them
+		task_config.cRadioButtons = 0;
+		task_config.pRadioButtons = nullptr;
+		task_config.nDefaultRadioButton = 0;
+
+		task_config.pszCollapsedControlText = nullptr;
+		task_config.hFooterIcon = nullptr;
+		task_config.pszFooter = nullptr;
+		task_config.pszFooterIcon = nullptr;
+		task_config.cxWidth = 0;
+
+		/// Titles and descriptions
+		auto title_u16 = utf8_to_utf16(param.title);
+		auto description_u16 = utf8_to_utf16(param.description);
+		auto long_description_u16 = utf8_to_utf16(param.long_description);
+		auto checkbox_text_u16 = utf8_to_utf16(param.checkbox_text);
+		auto additional_info_u16 = utf8_to_utf16(param.additional_info);
+
+		task_config.pszWindowTitle = title_u16.c_str();
+		//AssumeBetween(int(icon), int(MessageBoxIcon::Warning), int(MessageBoxIcon::Security));
+		task_config.pszMainIcon = MAKEINTRESOURCEW(int(param.icon));
+		task_config.pszMainInstruction = description_u16.c_str();
+		if (!param.long_description.empty())
+			task_config.pszContent = long_description_u16.c_str();
+
+		if (!param.checkbox_text.empty())
+			task_config.pszVerificationText = checkbox_text_u16.c_str();
+		if (!param.additional_info.empty())
+		{
+			task_config.pszExpandedControlText = L"Additional Information";
+			task_config.pszExpandedInformation = additional_info_u16.c_str();
+		}
+
+		/// Callback
+		if (param.callback)
+		{
+			task_config.pfCallback = [](HWND hwnd, UINT uNotification, WPARAM wParam, LPARAM lParam, LONG_PTR dwRefData) {
+				auto ptr = (decltype(param.callback)*)dwRefData;
+				return ptr->operator()((windows_message_box_event)uNotification, wParam, lParam) ? S_OK : S_FALSE;
+			};
+			task_config.lpCallbackData = (LONG_PTR)&param.callback;
+		}
+
+		/// Buttons
+		std::vector<TASKDIALOG_BUTTON> button_vector;
+		std::vector<std::wstring> button_texts;
+		std::transform(param.buttons.begin(), param.buttons.end(), std::back_inserter(button_texts), utf8_to_utf16);
+		std::transform(button_texts.begin(), button_texts.end(), std::back_inserter(button_vector), [id = 0](auto& wstr) mutable{ return TASKDIALOG_BUTTON{ id++, wstr.c_str() }; });
+		/*int id = 0;
+		for (auto& button : button_texts)
+			button_vector.push_back({ id++, button.c_str() });
+			*/
+
+		task_config.cButtons = (UINT)button_vector.size();
+		task_config.pButtons = button_vector.data();
+		task_config.nDefaultButton = (int)param.default_button;
+
+		int clicked_id = 0;
+		BOOL checkbox_value = false;
+		InitCommonControls();
+		auto result = TaskDialogIndirect(&task_config, &clicked_id, nullptr, &checkbox_value);
+		if (result != S_OK)
+		{
+			return { true };
+		}
+
+		return{ false, size_t(clicked_id), checkbox_value != 0 };
+	}
+
+}
---
+++ windows_message_box.h
@@ -0,0 +1,173 @@
+#pragma once
+
+#include <string_view>
+#include <string>
+#include <functional>
+#include <span>
+
+namespace ghassanpl
+{
+	struct windows_message_box_result;
+	struct windows_message_box_params;
+
+	enum class windows_message_box_event;
+
+	/// Basic API
+
+	/// Enum: windows_message_box_icon
+	/// Type of icon in the message box. If not given, `Information` will be used by default.
+	/// 
+	/// Warning - An exclamation-point icon
+	/// Error - A stop-sign icon
+	/// Information - An icon consisting of a lowercase letter i in a circle
+	/// Security - A shield icon
+	enum class windows_message_box_icon
+	{
+		Warning = -1,
+		Error = -2,
+		Information = -3,
+		Security = -4
+	};
+
+	namespace msg
+	{
+		/// Title of the message box window. This is "Message" by default.
+		struct title { std::string_view str; };
+
+		/// The primary instruction to the user (the main header of the message box)
+		struct description { std::string_view str; };
+
+		/// The longer description of the message (the smaller text of the message box).
+		/// This text can contain hyperlinks (`<A HREF="">asd</A>`) which will trigger the callback function, if given.
+		struct long_description { std::string_view str; };
+
+		/// If a non-empty checkbox_text is given, a checkbox will be present in the message box. The message box result will specify whether or not it was checked.
+		/// The callback, if present, will also be called when the checkbox state changes.
+		struct checkbox_text { std::string_view str; };
+
+		/// If a non-empty additional_info is given, a collapsible sub-section will be present in the message box, containg the text given in the additional info.
+		/// This text can contain hyperlinks (`<A HREF="">asd</A>`) which will trigger the callback function, if given.
+		struct additional_info { std::string_view str; };
+
+		/// If given, the button with this name will be selected by default
+		struct default_button { std::string_view str; };
+
+		/// If given, the message box will be modal to this window. Must be intitalized with a value of type <c>static_cast&lt;void*&gt;(HWND)</c>.
+		struct window_handle { void* handle; };
+	}
+
+	/// Function: windows_message_box
+	/// The primary internal workhorse function. Using the variadic version instead of this is preferable.
+	/// 
+	/// Parameters:
+	///		param - Structure holding the parameters of the message box
+	/// Returns:
+	///		The result of the user interacting with the message box
+	windows_message_box_result windows_message_box(windows_message_box_params const& param);
+
+	/// Function: windows_message_box
+	/// Primary function to display the message box
+	/// 
+	/// Parameters:
+	///		title - Title of the message box window
+	///		description - The primary instruction to the user (the main header of the message box)
+	///		args - Additional parameters to tweak the message box behavior. They can be:
+	/// 
+	/// - one of the types in namespace msg
+	/// - a <windows_message_box_icon> (::Warning, ::Error, ::Information or ::Security) specifying the type of icon to display. This is ::Information by default
+	/// - a range convertible to a range of string_views - these will be used as the names of the buttons to display in the message box. The default is a single "OK" button
+	/// - a size_t 0-based number of the button that should be highlighted by default. If not given, the 0th button is highlighted
+	/// - a function of signature `bool(<windows_message_box_event>, uintptr_t, uintptr_t)` that will be used as a callback, and called when different events occur during the user interaction. See `windows_message_box_event` for more info
+	/// Returns:
+	///		The result of the user interacting with the message box. See <windows_message_box_result> for more info.
+	template <typename... ARGS>
+	windows_message_box_result windows_message_box(std::string_view title, std::string_view description, ARGS&&... args)
+	{
+		return ::ghassanpl::windows_message_box({ msg::title{title}, msg::description{description}, std::forward<ARGS>(args)... });
+	}
+
+	/// Struct: windows_message_box_result
+	/// Holds the results of the windows_message_box invocation
+	struct windows_message_box_result
+	{
+		/// Field: Failed
+		/// Will be true if the message box was closed via the "X" button or failed to appear (perhaps due to argument errors)
+		bool Failed = false;
+
+		/// Field: ClickedButton
+		/// The number of the button that was clicked
+		size_t ClickedButton = 0;
+
+		/// Field: CheckboxValue
+		/// Whether or not the checkbox was checked
+		bool CheckboxValue = false;
+
+		explicit operator bool() const noexcept { return !Failed; }
+		operator size_t() const noexcept { return Failed ? -1 : ClickedButton; }
+	};
+
+	/// Class: windows_message_box_event
+	enum class windows_message_box_event
+	{
+		DialogCreated = 0,
+		ButtonClicked = 2, /// param1 = (int)button id
+		LinkClicked = 3, /// param1 = (const wchar_t*)href
+		DialogDestroyed = 5,
+		CheckboxClicked = 8, /// param2 = (bool)checked
+		HelpRequested = 9,
+	};
+
+	/// Struct: windows_message_box_params
+	/// Holds all the parameters for the message box. Prefer to use the variadic version of <windows_message_box>
+	struct windows_message_box_params
+	{
+		static inline constexpr std::string_view default_buttons[1] = { "OK" };
+
+		std::string_view title = "Message";
+		windows_message_box_icon icon = windows_message_box_icon::Information;
+		std::string_view description{};
+		std::span<std::string_view const> buttons = default_buttons;
+		std::vector<std::string_view> buttons_storage;
+		size_t default_button = 0;
+		std::string_view default_button_str{};
+		std::string_view long_description{};
+		std::string_view checkbox_text{};
+		std::string_view additional_info{};
+		std::function<bool(windows_message_box_event, uintptr_t, uintptr_t)> callback{};
+		void* window_handle = nullptr;
+
+		template <typename... ARGS>
+		windows_message_box_params(ARGS&&... args)
+		{
+			(Set(std::forward<ARGS>(args)), ...);
+		}
+
+		void Set(msg::title param) { title = param.str; }
+
+		void Set(windows_message_box_icon param) { icon = param; }
+
+		void Set(msg::description param) { description = param.str; }
+		void Set(std::string_view desc) { description = desc; }
+
+		void Set(std::span<std::string_view const> param) { buttons = param; }
+
+		template <typename T>
+		requires (std::constructible_from<decltype(buttons_storage), decltype(std::begin(std::declval<T>())), decltype(std::end(std::declval<T>()))> && !std::constructible_from<decltype(buttons), T>)
+		void Set(T param) { buttons_storage = std::vector<std::string_view>{ std::begin(param), std::end(param) }; buttons = buttons_storage; }
+
+		void Set(msg::default_button param) { default_button_str = param.str; }
+		void Set(size_t param) { default_button = param; }
+
+		void Set(msg::long_description param) { long_description = param.str; }
+
+		void Set(msg::checkbox_text param) { checkbox_text = param.str; }
+
+		void Set(msg::additional_info param) { additional_info = param.str; }
+
+		void Set(std::function<bool(windows_message_box_event, uintptr_t, uintptr_t)> param) { callback = std::move(param); }
+		// TODO: void Set(std::function<bool(windows_message_box_event, int, std::string_view)> param) { callback = std::move(param); }
+
+		void Set(msg::window_handle param) { window_handle = param.handle; }
+	};
+
+}
